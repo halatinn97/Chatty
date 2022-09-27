6bc3edb6e82834687cbbea4118e175d45337459d
Hala Tinn <hala79tinn@gmail.com>
2022-09-27 21:18:34 +0200

    Username + background features

--- components/Chat.js
+++ components/Chat.js
@@ -1,13 +1,35 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, Text, View, TextInput, ScrollView } from 'react-native';
+import 'react-native-gesture-handler';
 
 
 export default class Chat extends React.Component {
-    render() {
-        return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>Chat here</Text>
-            </View>
-        )
+    componentDidMount() {
+        let { name } = this.props.route.params;
+        this.props.navigation.setOptions({ title: name });
     }
+
+
+    render() {
+        const { color } = this.props.route.params;
+        return (
+            <ScrollView contentContainerStyle={[{ backgroundColor: color }, styles.container]}>
+                <View>
+                    <Text style={styles.chatTitle}>Chat here</Text>
+                </View>
+            </ScrollView>
+        );
+    };
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    chatTitle: {
+        color: '#FFFFFF'
+    }
+})

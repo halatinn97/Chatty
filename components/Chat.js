52aa893530453f645692401a99480eef25aba71f
Hala Tinn <hala79tinn@gmail.com>
2022-09-28 14:10:27 +0200

    Create chat interface

--- components/Chat.js
+++ components/Chat.js
@@ -1,23 +1,62 @@
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, ScrollView } from 'react-native';
 import 'react-native-gesture-handler';
+import { GiftedChat } from 'react-native-gifted-chat'
 
 
 export default class Chat extends React.Component {
-    componentDidMount() {
-        let { name } = this.props.route.params;
-        this.props.navigation.setOptions({ title: name });
+    constructor() {
+        super();
+        this.state = {
+            messages: [],
+        }
     }
 
+    componentDidMount() {
+
+        this.setState({
+            messages: [
+                {
+                    _id: 1,
+                    text: 'Hello developer',
+                    createdAt: new Date(),
+                    user: {
+                        _id: 2,
+                        name: 'React Native',
+                        avatar: 'https://placeimg.com/140/140/any',
+                    },
+                },
+                {
+                    _id: 2,
+                    text: 'This is a sytem message',
+                    createdAt: new Date(),
+                    system: true
+                }
+            ],
+        })
+    }
+    onSend(messages = []) {
+        this.setState(previousState => ({
+            messages: GiftedChat.append(previousState.messages, messages),
+        }))
+    }
 
     render() {
         const { color } = this.props.route.params;
+
+        let { name } = this.props.route.params;
+        this.props.navigation.setOptions({ title: name });
+
         return (
-            <ScrollView contentContainerStyle={[{ backgroundColor: color }, styles.container]}>
-                <View>
-                    <Text style={styles.chatTitle}>Chat here</Text>
-                </View>
-            </ScrollView>
+            <View style={[{ backgroundColor: color }, styles.container]}>
+                <GiftedChat
+                    messages={this.state.messages}
+                    onSend={messages => this.onSend(messages)}
+                    user={{
+                        _id: 1,
+                    }}
+                />
+            </View>
         );
     };
 }
@@ -25,9 +64,6 @@ export default class Chat extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
     },
     chatTitle: {
         color: '#FFFFFF'

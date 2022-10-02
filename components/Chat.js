32c5ad36186bfb68b748f79171bc115fc805c705
Hala Tinn <hala79tinn@gmail.com>
2022-10-02 21:45:13 +0200

    Import CustomActions & MapView

--- components/Chat.js
+++ components/Chat.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { StyleSheet, View, KeyboardAvoidingView } from 'react-native';
 import 'react-native-gesture-handler';
 import { GiftedChat, Bubble, InputToolbar } from 'react-native-gifted-chat'
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import NetInfo from '@react-native-community/netinfo';
+import CustomActions from './CustomActions';
+import { MapView } from 'expo';
 
 
 // Import functions from SDKs
@@ -23,6 +25,7 @@ export default class Chat extends React.Component {
                 name: '',
             },
             isConnected: false,
+            image: null,
         };
 
         //Set up Firebase
@@ -59,6 +62,8 @@ export default class Chat extends React.Component {
                     name: data.user.name,
                     avatar: data.user.avatar,
                 },
+                image: data.image || null,
+                location: data.location || null,
             });
         });
         this.setState({
@@ -156,6 +161,8 @@ export default class Chat extends React.Component {
             text: message.text,
             createdAt: message.createdAt,
             user: message.user,
+            image: data.image || null,
+            location: data.location || null,
         });
     }
 
@@ -192,6 +199,30 @@ export default class Chat extends React.Component {
         }
     }
 
+    renderCustomActions = (props) => {
+        return <CustomActions {...props} />;
+    };
+
+
+    //Returns MapView if message contains location data
+    renderCustomView(props) {
+        const { currentMessage } = props;
+        if (currentMessage.location) {
+            return (
+                <MapView
+                    style={{ width: 150, height: 100, borderRadius: 13, margin: 3 }}
+                    region={{
+                        latitude: currentMessage.location.latitude,
+                        longitude: currentMessage.location.longitude,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421,
+                    }}
+                />
+            );
+        }
+        return null;
+    }
+
 
     render() {
         const { color, name } = this.props.route.params;
@@ -202,11 +233,15 @@ export default class Chat extends React.Component {
                     renderBubble={this.renderBubble.bind(this)}
                     renderInputToolbar={this.renderInputToolbar.bind(this)}
                     messages={this.state.messages}
+                    isConnected={this.state.isConnected}
                     onSend={messages => this.onSend(messages)}
                     user={{
                         _id: this.state.user._id,
                         name: name,
                     }}
+                    renderActions={this.renderCustomActions}
+                    renderCustomView={this.renderCustomView}
+
                 />
                 {/*Prevent hidden input field on Android*/}
                 {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
